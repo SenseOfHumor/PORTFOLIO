62e88b18689d864551bc3c84d5d5a3b710d3309c
SWAPNIL <swa2314@gmail.com>
2025-08-15 20:26:30 -0400

    test_deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   assetsInclude: ['**/*.glb'],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/', // change to '/' if username.github.io repo
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.glb'],
 })
